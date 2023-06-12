89b215e38a68460771d84fd11e83ad905ca83452
AlexGeorge42580043 <georgealex42580043@gmail.com>
2023-06-12 00:33:58 -0300

    Creacion de interfaz grafica

--- Ejercicio4 TP4/Ejercicio4 TP4/Form1.Designer.vb
+++ Ejercicio4 TP4/Ejercicio4 TP4/Form1.Designer.vb
@@ -22,10 +22,59 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Button1 = New Button()
+        grMostrarVector = New DataGridView()
+        Label1 = New Label()
+        CType(grMostrarVector, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(27, 64)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(743, 109)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' grMostrarVector
+        ' 
+        grMostrarVector.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        grMostrarVector.ColumnHeadersVisible = False
+        grMostrarVector.EnableHeadersVisualStyles = False
+        grMostrarVector.Location = New Point(27, 236)
+        grMostrarVector.Name = "grMostrarVector"
+        grMostrarVector.RowHeadersVisible = False
+        grMostrarVector.RowHeadersWidth = 51
+        grMostrarVector.RowTemplate.Height = 29
+        grMostrarVector.Size = New Size(743, 79)
+        grMostrarVector.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(27, 213)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(509, 20)
+        Label1.TabIndex = 2
+        Label1.Text = "Vector formado por los minimos valores de cada fila de la matriz ingresada"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label1)
+        Controls.Add(grMostrarVector)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(grMostrarVector, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents grMostrarVector As DataGridView
+    Friend WithEvents Label1 As Label
 End Class
